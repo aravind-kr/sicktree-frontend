87b4e989a652d7318ab15e5699f10132351692d9
Arvind <arvindsmvec@gmail.com>
2018-11-29 15:09:10 +0530

    border color in theme

--- components/theme.js
+++ components/theme.js
@@ -9,6 +9,7 @@ const globalTheme = {
     cherryRedHighlight: '#980A0A',
     cherryPinkText: '#DD4E44',
     bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+    borderColor: 'rgba(0, 0, 0, 0.09)'
 }
 
 export default globalTheme;
