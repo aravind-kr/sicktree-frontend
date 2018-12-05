353f8c90ba4f5de875d484fe8d1df60abdeed443
Arvind <arvindsmvec@gmail.com>
2018-12-05 22:45:06 +0530

    index layout changed

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 import Banner from '../components/index/banner';
+import HIW from '../components/index/howitworks';
+import Description from '../components/index/description';
 
-const Home = prop => <Banner />;
+const Home = prop => (
+    <>
+        <Banner />
+        <HIW />
+        <Description />
+    </>
+);
 
 export default Home;
